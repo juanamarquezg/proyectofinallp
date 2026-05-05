f32c5e7bc66ca77bc30cfdddd6d028f43c0d0655
juanamarquezg <juanam0308@gmail.com>
2026-05-05 15:52:12 -0500

    MovimientoPersonaje

---
+++ Assets/movplayer.cs
@@ -0,0 +1,82 @@
+using UnityEngine;
+
+
+public class movplayer : MonoBehaviour
+{
+    public float veloMove;
+    public float x;
+    public float fuerzaDeSalto;
+    public float y;
+
+    public Animator animacaos;
+
+    private bool golpeando = false;
+    private float tiempoGolpe = 0f;
+    public float duracionGolpe = 0.25f; 
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+     
+        x = Input.GetAxisRaw("Horizontal");
+        transform.Translate(Vector3.right * x * veloMove * Time.deltaTime);
+
+        if (x == 0)
+            animacaos.SetBool("correh", false);
+        else
+        {
+            animacaos.SetBool("correh", true);
+
+            if (x < 0)
+                transform.localScale = new Vector3(1, 1, 1);
+            else if (x > 0)
+                transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+     
+        y = Input.GetAxisRaw("Vertical");
+        transform.Translate(Vector3.up * y * veloMove * Time.deltaTime);
+
+        animacaos.SetBool("correvup", false);
+        animacaos.SetBool("correvdo", false);
+
+        if (y > 0)
+            animacaos.SetBool("correvup", true);
+        else if (y < 0)
+            animacaos.SetBool("correvdo", true);
+
+        
+        if (Input.GetKeyDown(KeyCode.F) && !golpeando)
+        {
+            animacaos.SetTrigger("golpe");
+            golpeando = true;
+            tiempoGolpe = duracionGolpe;
+        }
+
+        if (golpeando)
+        {
+            tiempoGolpe -= Time.deltaTime;
+
+            if (tiempoGolpe <= 0)
+            {
+                golpeando = false;
+            }
+        }
+    }
+
+    private void OnTriggerstay2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemigo") && golpeando)
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
